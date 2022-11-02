3cbbc42d86e5274007f4e4f9485284a6ce127444
Fabricio Miranda <fbrnery@gmail.com>
2022-11-01 22:59:51 -0300

    Validação e teste unitário

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
-    has_one_attached :featured_image
+   has_one_attached :featured_image
+   validates :title, :description, presence: true
+   validates :title, uniqueness: true
+   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
