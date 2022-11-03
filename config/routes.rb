2eade163fdfd0a70a86b70079095fffff0eafb23
Fabricio Miranda <fbrnery@gmail.com>
2022-11-03 15:46:30 -0300

    Reorganizei o Header da aplicação

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
   get 'store/index'
+  get 'store/contatos'
+  get 'store/suporte'
+  get 'store/loja'
+
+
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
